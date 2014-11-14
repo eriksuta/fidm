2d144a1ade7068f91259c891a07483a31b5f425d
Erik Suta <erik.suta@evolveum.com>
2014-11-14 22:51:56 +0100

    Initial commit - wicket project with maven + embedded jetty server

---
+++ fidm/src/main/java/com/esuta/fidm/WicketApplication.java
@@ -0,0 +1,30 @@
+package com.esuta.fidm;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ *  @author shood
+ * */
+public class WicketApplication extends WebApplication
+{
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	@Override
+	public Class<? extends WebPage> getHomePage()
+	{
+		return HomePage.class;
+	}
+
+	/**
+	 * @see org.apache.wicket.Application#init()
+	 */
+	@Override
+	public void init()
+	{
+		super.init();
+
+        //TODO - add initial configuration here
+	}
+}
