d2242d9375cbb08a62b78436ac12daadeba6df61
Erik Suta <erik.suta@evolveum.com>
2015-01-17 22:54:07 +0100

    Adding basic GUI handling for resources

--- fidm/src/main/java/com/esuta/fidm/gui/page/resource/PageResource.java
+++ fidm/src/main/java/com/esuta/fidm/gui/page/resource/PageResource.java
@@ -1,13 +1,151 @@
 package com.esuta.fidm.gui.page.resource;
 
+import com.esuta.fidm.gui.component.model.LoadableModel;
 import com.esuta.fidm.gui.page.PageBase;
+import com.esuta.fidm.infra.exception.DatabaseCommunicationException;
+import com.esuta.fidm.infra.exception.GeneralException;
+import com.esuta.fidm.model.ModelService;
+import com.esuta.fidm.repository.schema.ResourceType;
+import org.apache.log4j.Logger;
+import org.apache.wicket.RestartResponseException;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 /**
  *  @author shood
  * */
 public class PageResource extends PageBase {
 
-    public PageResource(){
+    private transient Logger LOGGER = Logger.getLogger(PageResource.class);
 
+    public static final String ID_MAIN_FORM = "mainForm";
+    public static final String ID_NAME = "name";
+    public static final String ID_DESCRIPTION = "description";
+    public static final String ID_TYPE = "type";
+    public static final String ID_BUTTON_SAVE = "saveButton";
+    public static final String ID_BUTTON_CANCEL = "cancelButton";
+
+    private IModel<ResourceType> model;
+
+    public PageResource(){
+        this(null);
+    }
+
+    public PageResource(PageParameters parameters){
+        super(parameters);
+
+        model = new LoadableModel<ResourceType>(false) {
+
+            @Override
+            protected ResourceType load() {
+                return loadResource();
+            }
+        };
+
+        initLayout();
+    }
+
+    private ResourceType loadResource(){
+        if(!isEditingResource()){
+            return new ResourceType();
+        }
+
+        PageParameters parameters = getPageParameters();
+        String uid = parameters.get(UID_PAGE_PARAMETER_NAME).toString();
+        ResourceType resource;
+
+        try {
+            resource = getModelService().readObject(ResourceType.class, uid);
+        } catch (DatabaseCommunicationException exc){
+            error("Couldn't retrieve resource with oid: '" + uid + "' from the repository. Reason: " + exc.getExceptionMessage());
+            LOGGER.error("Couldn't retrieve resource with oid: '" + uid + "' from the repository. Reason: ", exc);
+            throw new RestartResponseException(PageResourceList.class);
+        }
+
+        return resource;
+    }
+
+    private boolean isEditingResource(){
+        PageParameters parameters = getPageParameters();
+        return !parameters.get(UID_PAGE_PARAMETER_NAME).isEmpty();
+    }
+
+    private void initLayout(){
+        Form mainForm = new Form(ID_MAIN_FORM);
+        mainForm.setOutputMarkupId(true);
+        add(mainForm);
+
+        TextField name = new TextField<>(ID_NAME, new PropertyModel<String>(model, "name"));
+        name.setRequired(true);
+        mainForm.add(name);
+
+        TextArea description = new TextArea<>(ID_DESCRIPTION, new PropertyModel<String>(model, "description"));
+        mainForm.add(description);
+
+        TextField type = new TextField<>(ID_TYPE, new PropertyModel<String>(model, "resourceType"));
+        mainForm.add(type);
+
+        AjaxSubmitLink cancel = new AjaxSubmitLink(ID_BUTTON_CANCEL) {
+
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                cancelPerformed();
+            }
+        };
+        cancel.setDefaultFormProcessing(false);
+        mainForm.add(cancel);
+
+        AjaxSubmitLink save = new AjaxSubmitLink(ID_BUTTON_SAVE) {
+
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                savePerformed(target);
+            }
+
+            @Override
+            protected void onError(AjaxRequestTarget target, Form<?> form) {
+                target.add(getFeedbackPanel());
+            }
+        };
+        mainForm.add(save);
+    }
+
+    private void cancelPerformed(){
+        setResponsePage(PageResourceList.class);
+    }
+
+    private void savePerformed(AjaxRequestTarget target){
+        ModelService modelService = getModelService();
+        ResourceType resource;
+
+        if(model == null || model.getObject() == null){
+            return;
+        }
+
+        resource = model.getObject();
+
+        try{
+
+            if(!isEditingResource()){
+                modelService.createObject(resource);
+            } else {
+                modelService.updateObject(resource);
+            }
+
+        } catch (GeneralException e){
+            LOGGER.error("Can't add resource: ", e);
+            error("Can't add resource with name: '" + resource.getName() + "'. Reason: " + e.getExceptionMessage());
+        }
+
+        getSession().success("Resource '" + resource.getName() + "' has been saved successfully.");
+        LOGGER.info("Resource '" + resource.getName() + "' has been saved successfully.");
+        setResponsePage(PageResourceList.class);
+        target.add(getFeedbackPanel());
     }
 }
--- fidm/src/main/java/com/esuta/fidm/gui/page/resource/PageResourceList.java
+++ fidm/src/main/java/com/esuta/fidm/gui/page/resource/PageResourceList.java
@@ -1,13 +1,113 @@
 package com.esuta.fidm.gui.page.resource;
 
+import com.esuta.fidm.gui.component.data.ObjectDataProvider;
+import com.esuta.fidm.gui.component.data.column.EditDeleteButtonColumn;
+import com.esuta.fidm.gui.component.data.table.TablePanel;
 import com.esuta.fidm.gui.page.PageBase;
+import com.esuta.fidm.infra.exception.GeneralException;
+import com.esuta.fidm.repository.schema.ResourceType;
+import org.apache.log4j.Logger;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *  @author shood
  * */
 public class PageResourceList extends PageBase {
 
-    public PageResourceList(){
+    private transient Logger LOGGER = Logger.getLogger(PageResourceList.class);
 
+    private static final String ID_MAIN_FORM = "mainForm";
+    private static final String ID_TABLE = "table";
+
+    public PageResourceList(){
+        initLayout();
+    }
+
+    private void initLayout(){
+        Form form = new Form(ID_MAIN_FORM);
+        form.setOutputMarkupId(true);
+        add(form);
+
+        List<IColumn> columns = createColumns();
+        ObjectDataProvider<ResourceType> provider = new ObjectDataProvider<>(getPage(), ResourceType.class);
+
+        TablePanel table = new TablePanel(ID_TABLE, provider, columns, 10);
+        table.setOutputMarkupId(true);
+        form.add(table);
+    }
+
+    private List<IColumn> createColumns(){
+        List<IColumn> columns = new ArrayList<>();
+
+        columns.add(new PropertyColumn<ResourceType, String>(new Model<>("Name"), "name", "name"));
+        columns.add(new PropertyColumn<ResourceType, String>(new Model<>("Type"), "resourceType", "resourceType"));
+
+        columns.add(new EditDeleteButtonColumn<ResourceType>(new Model<>("Actions")){
+
+            @Override
+            public void editPerformed(AjaxRequestTarget target, IModel<ResourceType> rowModel) {
+                PageResourceList.this.editPerformed(target, rowModel);
+            }
+
+            @Override
+            public void removePerformed(AjaxRequestTarget target, IModel<ResourceType> rowModel) {
+                PageResourceList.this.removePerformed(target, rowModel);
+            }
+        });
+
+        return columns;
+    }
+
+    private TablePanel getTablePanel(){
+        return (TablePanel) get(ID_MAIN_FORM + ":" + ID_TABLE);
+    }
+
+    private Form getMainForm(){
+        return (Form) get(ID_MAIN_FORM);
+    }
+
+    private void editPerformed(AjaxRequestTarget target, IModel<ResourceType> rowModel){
+        if(rowModel == null || rowModel.getObject() == null){
+            error("Couldn't edit selected resource. It is no longer available.");
+            target.add(getFeedbackPanel());
+            return;
+        }
+
+        PageParameters parameters = new PageParameters();
+        parameters.add(UID_PAGE_PARAMETER_NAME, rowModel.getObject().getUid());
+        setResponsePage(new PageResource(parameters));
+    }
+
+    private void removePerformed(AjaxRequestTarget target, IModel<ResourceType> rowModel){
+        if(rowModel == null || rowModel.getObject() == null){
+            error("Object selected to delete does not exist.");
+            target.add(getFeedbackPanel());
+            return;
+        }
+
+        ResourceType role = rowModel.getObject();
+        String roleName = role.getName();
+
+        try {
+            getModelService().deleteObject(role);
+        } catch (GeneralException e){
+            LOGGER.error("Could not delete resource: '" + roleName + "'. Reason: ", e);
+            error("Could not delete resource: '" + roleName + "'. Reason: " + e.getExceptionMessage());
+            target.add(getFeedbackPanel());
+            return;
+        }
+
+        LOGGER.info("Resource '" + roleName + "' was successfully deleted from the system.");
+        success("Resource '" + roleName + "' was successfully deleted from the system.");
+        target.add(getFeedbackPanel(), getMainForm());
     }
 }
