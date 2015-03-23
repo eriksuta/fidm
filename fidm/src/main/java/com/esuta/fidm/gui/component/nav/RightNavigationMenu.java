235b45d0d365613ad455eac177669ec8606bd38e
erik <erik.suta@evolveum.com>
2015-03-23 22:05:14 +0100

    Adding template for new page - sharing policy configuration

--- fidm/src/main/java/com/esuta/fidm/gui/component/nav/RightNavigationMenu.java
+++ fidm/src/main/java/com/esuta/fidm/gui/component/nav/RightNavigationMenu.java
@@ -9,6 +9,7 @@ import com.esuta.fidm.gui.page.federation.PageFederation;
 import com.esuta.fidm.gui.page.federation.PageFederationList;
 import com.esuta.fidm.gui.page.org.PageOrg;
 import com.esuta.fidm.gui.page.org.PageOrgList;
+import com.esuta.fidm.gui.page.org.PageSharingPolicy;
 import com.esuta.fidm.gui.page.resource.PageResource;
 import com.esuta.fidm.gui.page.resource.PageResourceList;
 import com.esuta.fidm.gui.page.roles.PageRole;
@@ -83,6 +84,7 @@ public class RightNavigationMenu extends Panel {
         NavigationMenuTopLevelItem orgSection = new NavigationMenuTopLevelItem(createStringResource("page.link.org"), "fa fa-sitemap fa-fw", null);
         orgSection.addSecondLevelItem(new NavigationMenuSecondLevelItem(createStringResource("page.link.org.new"), PageOrg.class));
         orgSection.addSecondLevelItem(new NavigationMenuSecondLevelItem(createStringResource("page.link.org.list"), PageOrgList.class));
+        orgSection.addSecondLevelItem(new NavigationMenuSecondLevelItem(createStringResource("page.link.org.policy"), PageSharingPolicy.class));
         menuItemList.add(orgSection);
 
         //init Federation section
---
+++ fidm/src/main/java/com/esuta/fidm/gui/page/org/PageSharingPolicy.java
@@ -0,0 +1,11 @@
+package com.esuta.fidm.gui.page.org;
+
+import com.esuta.fidm.gui.page.PageBase;
+
+/**
+ *  @author shood
+ * */
+public class PageSharingPolicy extends PageBase{
+
+    public PageSharingPolicy(){}
+}
